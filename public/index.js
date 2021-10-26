926d0c056f760f36bc79b62e3e5cce1200e62aef
SergeyKhrapin <khrapins@gmail.com>
2021-10-26 17:38:54 +0300

    add error handling

--- public/index.js
+++ public/index.js
@@ -4,17 +4,24 @@ const button = document.getElementById('fetch')
 button.addEventListener('click', fetchUsers)
 
 async function fetchUsers() {
-    const response = await fetch('/users')
-    const { data } = await response.json()    
-    const userList = document.createElement('ul')
+	try {
+		// const response = await fetch('/users/blablabla') // 404
+		const response = await fetch('/users')
+		const { data } = await response.json()
+		const userList = document.createElement('ul')
 
-    data.map(user => {
-        const userItem = document.createElement('li')
-        userItem.innerText = user.firstName + ' ' + user.lastName
-        userList.append(userItem)
-    })
+		data.map(user => {
+				const userItem = document.createElement('li')
+				userItem.innerText = user.firstName + ' ' + user.lastName
+				userList.append(userItem)
+		})
 
-    // TODO: to improve a performance (reflow/repaint) 
-    button.remove()
-    users.append(userList)
+		// TODO: to improve a performance (reflow/repaint)
+		button.remove()
+		users.append(userList)
+	} catch(e) {
+		// Express recognizes 404 like an error
+		// In fact, 404 is not an error, and catch block does not have to be executed
+		console.log('CATCH')
+	}
 }
